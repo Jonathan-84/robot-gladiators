53fe09cc69a0d56fbc768edd0de442cff3972ac0
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-10-29 10:22:36 -0400

    cycle through enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,8 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(enemyName) {
+  var fight = function(enemyName) {
+    while(enemyHealth > 0) {
 //console.log(enemyNames[0]);
 //console.log(enemyNames[1]);
 //console.log(enemyNames[2]);
@@ -18,18 +19,16 @@ var fight = function(enemyName) {
   //console.log(enemyNames[i]);
   //console.log( i);
   //console.log(enemyNames[i] + " is at" +  i + " index ");
-    window.alert("Welcome to Robot Gladiators!");
+    //window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt ( "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    
-// if player choses to fight, then fight
+    // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
-  
     // check enemy's health
     if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
@@ -49,11 +48,11 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+    // if enemy has health points, we keep fighting
+   
+
     // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
-  } 
-  if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
   
@@ -75,6 +74,10 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     //*Defeat each enemy robot
 //"LOSE - PLayer robot's health is zero or less
   };
+}
   for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    // call fight function with enemy-robot
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
   }
